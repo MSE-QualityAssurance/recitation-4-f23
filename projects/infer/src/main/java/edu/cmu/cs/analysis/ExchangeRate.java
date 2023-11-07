27971d6dfb9d0e59948464703ec1471428cccdb9
Paulo Santos <pcanelasdossantos@gmail.com>
2023-11-07 16:24:19 -0500

    Updated java files

--- projects/infer/src/main/java/edu/cmu/cs/analysis/ExchangeRate.java
+++
@@ -1,9 +0,0 @@
-package edu.cmu.cs.analysis;
-
-public class ExchangeRate {
-
-  public double getRate(String type1, String type2) {
-    throw new UnsupportedOperationException("Implement");
-  }
-
-}
