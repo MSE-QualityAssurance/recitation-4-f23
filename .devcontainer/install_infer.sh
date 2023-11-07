c23dec6c6962a0af400d9021e12459e0206a77e7
Paulo Santos <pcanelasdossantos@gmail.com>
2023-11-07 16:56:17 -0500

    Fixed infer path

--- .devcontainer/install_infer.sh
+++ .devcontainer/install_infer.sh
@@ -7,7 +7,7 @@ wget https://github.com/facebook/infer/releases/download/v1.1.0/infer-linux64-v1
 sudo tar -xf infer-linux64-v1.1.0.tar.xz -C /opt
 
 # Set up Infer alias (this will only be available in the shell that runs this script)
-echo 'export PATH=/opt/infer-linux64-1.1.0/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=/opt/infer-linux64-v1.1.0/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
 # Remove the infer-linux
