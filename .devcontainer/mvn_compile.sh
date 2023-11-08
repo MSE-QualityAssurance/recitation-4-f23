9f33057e923ec9ac8129b49ec265a43c7398a6b1
Paulo Canelas <64093960+pcanelas@users.noreply.github.com>
2023-11-08 03:46:20 +0000

    Updated spotbugs pom and mvn_compile

--- .devcontainer/mvn_compile.sh
+++ .devcontainer/mvn_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd projects/ 
 cd infer && mvn clean compile package 
-cd pmd && mvn clean compile package 
-cd spotbugs && mvn clean compile package
+cd ../pmd && mvn clean compile package 
+cd ../spotbugs && mvn clean compile package
