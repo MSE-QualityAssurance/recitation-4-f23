a185d04d83c29e82ebbdff7e1876fad5147b39b5
Paulo Santos <pcanelasdossantos@gmail.com>
2023-11-09 15:36:19 -0500

    Fixed .devcontainer

--- .devcontainer/mvn_compile.sh
+++ .devcontainer/mvn_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd projects/ 
 cd infer && mvn clean compile package 
-cd ../pmd-tools/torngest4ds/torngest && mvn clean compile package 
-cd ../../../spotbugs && mvn clean compile package
+cd ../pmd-tools/ && mvn clean compile package 
+cd ../spotbugs && mvn clean compile package
