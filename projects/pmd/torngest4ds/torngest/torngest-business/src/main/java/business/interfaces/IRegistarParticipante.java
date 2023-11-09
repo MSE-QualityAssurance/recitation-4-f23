a53576f2f2b313134cae1242ea91347600f76675
Paulo Santos <pcanelasdossantos@gmail.com>
2023-11-08 22:38:28 -0500

    Removed project

--- projects/pmd/torngest4ds/torngest/torngest-business/src/main/java/business/interfaces/IRegistarParticipante.java
+++
@@ -1,8 +0,0 @@
-package business.interfaces;
-
-import facade.exceptions.TornGesException;
-
-public interface IRegistarParticipante {
-
-	public void registarParticipante(String designacaoTorneio, int idParticipante) throws TornGesException;
-}
